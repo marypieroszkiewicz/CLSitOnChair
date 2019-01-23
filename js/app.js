ecd8e4a7cf555b4c3cb47f038b7568652c6e53e3
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2019-01-23 16:22:40 +0100

    Start Project

--- js/app.js
+++ js/app.js
@@ -4,12 +4,21 @@ document.addEventListener('DOMContentLoaded', function() {
 /* ----- TOGGLE SUBMENU ----- */
 /* ---------------------------------------------------- */
 
-	const submenuToggle = document.querySelector('.menu__item');
-    const submenu = document.querySelector('.submenu');
-    submenuToggle.addEventListener('click', function(e) {
-        e.preventDefault();
-        submenu.classList.toggle('open--submenu');
-    });
+	// const submenuToggle = document.querySelector('.menu__item');
+ //    const submenu = document.querySelector('.submenu');
+ //    submenuToggle.addEventListener('click', function(e) {
+ //        e.preventDefault();
+ //        submenu.classList.toggle('open--submenu');
+ //    });
 
+
+/* ---------------------------------------------------- */
+/* ----- WYSUWANIE SUBMENU ----- */
+/* ---------------------------------------------------- */
+
+
+/* ---------------------------------------------------- */
+/* ----- SLIDER GALLERY ----- */
+/* ---------------------------------------------------- */
 
 });
